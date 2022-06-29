800817f60d4f97036896408abfdf331ca89cefba
Sagar5192 <69325601+Sagar5192@users.noreply.github.com>
2022-06-29 07:56:21 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,40 @@
+lex
+%{
+#include "y.tab.h"
+%}
+%%
+a return A;
+b return B;
+.|\n return yytext[0];
+%%
+
+
+yacc
+%{
+#include<stdio.h>
+#include<stdlib.h>
+%}
+%token NUM
+%left '+' '-'
+%left '*' '/'
+%%
+S:e {printf("result=%d\n",$$); return 0;}
+;
+e:e'+'e {$$=$1+$3;}
+|e'-'e {$$=$1-$3;}
+|e'*'e {$$=$1*$3;}
+|e'/'e {$$=$1/$3;}
+|'('e')' {$$=$2;}
+|NUM {$$=$1;}
+;
+%%
+main()
+{
+printf("enter expr\n");
+yyparse();
+}
+yyerror()
+{
+printf("error\n");
+exit(0);
+}
